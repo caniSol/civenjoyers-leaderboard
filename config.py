eb5036ff4c15219ae2042cca7fce2e5ab21f4427
cani <co@bty.works>
2026-09-04 13:09:27 +0200

    Rebuild project to be a django webapp to track the ELO in a website

--- config.py
+++
@@ -1,3 +0,0 @@
-PATH_TO_GAMES_CSV = "/home/cani/workspace/civenjoyers-leaderboard/data/games.csv"
-PATH_TO_LEADERBOARD_CSV = "/home/cani/workspace/civenjoyers-leaderboard/data/leaderboard.csv"
-STANDARD_ELO = 1000
--- test_read_csv.py
+++
@@ -1,6 +0,0 @@
-from functions.read_csv import read_csv
-
-cases = ["/path/to/data.csv"]
-
-for case in cases:
-    print(read_csv(case))
